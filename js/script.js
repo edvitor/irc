4cd6625b8214a16b84f597cc70b12155d01eb677
Edson Vitor <edsonvitornascimento@gmail.com>
2024-10-09 10:45:11 -0300

    tratamentos correção media query

--- js/script.js
+++ js/script.js
@@ -56,46 +56,40 @@ $('.slider-nav').slick({
     slidesToScroll: 1,
     dots: true,
     centerMode: true,
-    centerPadding: '60px',
+    centerPadding: '90px',
     focusOnSelect: true,
+    prevArrow: '<button type="button" class="slick-prev"><i class="fa-solid fa-angle-left"></i></button>',
+    nextArrow: '<button type="button" class="slick-next"><i class="fa-solid fa-angle-right"></i></i></button>',
     responsive: [
         {
-            breakpoint: 993,
+            breakpoint: 1024,
             settings: {
-              slidesToShow: 1,
+              slidesToShow: 3,
               dots: true,
+              centerMode: true,
               arrows: true,
-              centerPadding: '30px'
-            }
-          }
-      ]
-  });
-
-  $('.autoplay').slick({
-    slidesToShow: 3,
-    slidesToScroll: 1,
-    autoplay: true,
-    swipe: false,
-    touchMove: false,
-    autoplaySpeed: 1500,
-    responsive: [
-        {
-            breakpoint: 1401,
-            settings: {
-              slidesToShow: 2,
-              dots: true,
-              arrows: true,
+              centerPadding: '120px'
             }
           },
-        
         {
             breakpoint: 993,
             settings: {
               slidesToShow: 1,
               dots: true,
               arrows: true,
+              centerMode: true,
+              centerPadding: '10px'
+            }
+          },
+          {
+            breakpoint: 768,
+            settings: {
+              slidesToShow: 1,
+              dots: false,
+              arrows: true,
+              centerMode: true,
+              centerPadding: '10px'
             }
           }
       ]
   });
-
